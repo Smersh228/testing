aa7bed089e85fb6702d474cc851403930bcab94a
Mstislaw <89087089+Smersh228@users.noreply.github.com>
2023-03-18 13:32:57 +0300

    Add files via upload

---
+++ precache-manifest.aa8bacc5850d5b8f6a7ccf4347dfd16f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3868fe4193e7a4981cc3",
+    "url": "/testing/static/css/main.8b03076a.chunk.css"
+  },
+  {
+    "revision": "3868fe4193e7a4981cc3",
+    "url": "/testing/static/js/main.a3f38695.chunk.js"
+  },
+  {
+    "revision": "237dbb1e33882783d24f",
+    "url": "/testing/static/js/runtime~main.4b4d9c2d.js"
+  },
+  {
+    "revision": "99b1961c55d675e491f5",
+    "url": "/testing/static/js/2.8808d590.chunk.js"
+  },
+  {
+    "revision": "bd2db5e315108ebec595",
+    "url": "/testing/static/js/3.5b0ae9b5.chunk.js"
+  },
+  {
+    "revision": "f05fca43a86ae0e08e2576c672e6c27b",
+    "url": "/testing/index.html"
+  }
+];
